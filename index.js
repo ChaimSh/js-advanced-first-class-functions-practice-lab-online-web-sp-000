7bbdad12f6438fe5a58e9b6014003dbdf958bdac
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-05 21:08:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 const logDriverNames = function(drivers){
-  driver.forEach(function (driver){
+  drivers.forEach(function (driver){
     console.log(driver.name)
   })
 }
